b3c5dfdad37323e1524ae5e8b7afaf13065f55d8
Marian Stasiuk <mst@primexm.com>
2024-09-02 20:51:07 +0300

    lesson1 - configs, annotations

--- src/test/java/gui/lesson1/FirstTest.java
+++ src/test/java/gui/lesson1/FirstTest.java
@@ -1,11 +1,9 @@
 package gui.lesson1;
 
 import base.config.BaseTest;
-import com.codeborne.selenide.Selenide;
+
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
 
 public class FirstTest extends BaseTest {
 
